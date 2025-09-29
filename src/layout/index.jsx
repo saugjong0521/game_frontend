48e839afc6ec82005f36f7e9d4e84c9fda5ee22a
saugjong0521 <149376211+saugjong0521@users.noreply.github.com>
2025-09-29 17:26:18 +0900

    (20250929) fix_route_error

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,7 +1,7 @@
 import GameLayout from "./GameLayout";
 import GameTicketBuy from "./GameTicketBuy";
 import Home from "./Home";
-import LeaderBoardLayout from "./LeaderboardLayout";
+import LeaderBoardLayout from "./LeaderBoardLayout";
 import { FortuneLeaderBoard } from "./fortunefrenzy";
 import { SurvivalLeaderBoard } from "./survival";
 
